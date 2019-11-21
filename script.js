0856b13995a6b995a63beb06ab9dd79c414be32f
andrewreedy <reedyandrew11@gmail.com>
2019-11-20 22:45:09 -0500

    wokring buttons

--- script.js
+++ script.js
@@ -1,18 +1,36 @@
    
+function random(length) {
    
-   //var type = confirm("Upper case?")
+   
+    //var type = confirm("Upper case?")
     var length = prompt("Length of password? (must be between 8 and 128 characters)")
     var result = ""
     var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'*+,-./:;<=>?@[\]^_`~1234567890"
     var lower = "abcdefghijklmnopqrstuvwxyz!#$%&'*+,-./:;<=>?@[\]^_`~1234567890"
     var upperLength = upper.length
      
-    function random(length) {
-      for ( var i = 0; i < length; i++)  {
+    
+   for ( var i = 0; i < length; i++)  {
    result +=  upper.charAt(Math.floor(Math.random()* upperLength));
       }
-       return result;
- }
+       //return result;
+
+    document.getElementById("display").value = result;
+    
+}
     //console.log(random(1))
 
 alert("Your password is " + random(length))
+
+
+
+
+function copyPassword(){
+
+    document.getElementById("display").select();
+
+    document.execCommand("Copy");
+
+    alert("copied!");
+
+}
