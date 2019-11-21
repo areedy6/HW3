4c3b0aad3ce6b2f54e6ef94aa2e7959b58bfb055
andrewreedy <reedyandrew11@gmail.com>
2019-11-21 18:15:45 -0500

    working case

--- script.js
+++ script.js
@@ -2,19 +2,23 @@
 function random(length) {
    
    
-    //var type = confirm("Upper case?")
+    var type = confirm("Upper case?")
     var length = prompt("Length of password? (must be between 8 and 128 characters)")
     var result = ""
     var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ!#$%&'*+,-./:;<=>?@[\]^_`~1234567890"
     var lower = "abcdefghijklmnopqrstuvwxyz!#$%&'*+,-./:;<=>?@[\]^_`~1234567890"
     var upperLength = upper.length
      
-    
+    if (!type) {
+    for ( var i = 0; i < length; i++)  {
+    result +=  lower.charAt(Math.floor(Math.random()* upperLength));
+    }
+    } else {
    for ( var i = 0; i < length; i++)  {
    result +=  upper.charAt(Math.floor(Math.random()* upperLength));
       }
        //return result;
-
+    }
     document.getElementById("display").value = result;
     
 }
